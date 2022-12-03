b90db5ef5370e6ebbdaacdf419729233acc65d0d
Martin Smith <martinsmith1968@gmail.com>
2022-12-03 14:55:29 +0000

    Extensions test coverage

---
+++ QuickCalendar.Domain.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+using QuickCalendar.Domain.Extensions;
+using Xunit;
+
+namespace QuickCalendar.Domain.Tests.Extensions
+{
+    public class DateTimeExtensionsTests
+    {
+        [Theory]
+        [MemberData(nameof(MinOf_Data))]
+        public void MinOf_returns_the_expected_value(DateTime dt1, DateTime dt2, DateTime expected)
+        {
+            var result = DateTimeExtensions.MinOf(dt1, dt2);
+
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(MaxOf_Data))]
+        public void MaxOf_returns_the_expected_value(DateTime dt1, DateTime dt2, DateTime expected)
+        {
+            var result = DateTimeExtensions.MaxOf(dt1, dt2);
+
+            result.Should().Be(expected);
+        }
+
+        public static IEnumerable<object[]> MinOf_Data()
+        {
+            var today = DateTime.UtcNow.Date;
+
+            yield return new object[] { today, today, today };
+            yield return new object[] { today.AddHours(12), today, today };
+            yield return new object[] { today.AddHours(12), today.AddHours(14), today.AddHours(12) };
+            yield return new object[] { today.AddHours(12), today.AddHours(4), today.AddHours(4) };
+            yield return new object[] { today, DateTimeExtensions.CalendarMinValue, DateTimeExtensions.CalendarMinValue };
+            yield return new object[] { today, DateTimeExtensions.CalendarMaxValue, today };
+        }
+
+        public static IEnumerable<object[]> MaxOf_Data()
+        {
+            var today = DateTime.UtcNow.Date;
+
+            yield return new object[] { today, today, today };
+            yield return new object[] { today.AddHours(12), today, today.AddHours(12) };
+            yield return new object[] { today.AddHours(12), today.AddHours(14), today.AddHours(14) };
+            yield return new object[] { today.AddHours(12), today.AddHours(4), today.AddHours(12) };
+            yield return new object[] { today, DateTimeExtensions.CalendarMaxValue, DateTimeExtensions.CalendarMaxValue };
+            yield return new object[] { today, DateTimeExtensions.CalendarMinValue, today };
+        }
+    }
+}
---
+++ QuickCalendar.Domain.Tests/Extensions/NumericExtensionsTests.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using QuickCalendar.Domain.Extensions;
+using Xunit;
+
+namespace QuickCalendar.Domain.Tests.Extensions
+{
+    public class NumericExtensionsTests
+    {
+        [Theory]
+        [InlineData(123.45, 123)]
+        [InlineData(123.88, 124)]
+        [InlineData(123, 123)]
+        [InlineData(0, 0)]
+        [InlineData(-1, -1)]
+        [InlineData(-1.45, -1)]
+        [InlineData(-1.88, -2)]
+        public void Decimal_ToInt32_converts_as_expected(decimal value, int expected)
+        {
+            var result = value.ToInt32();
+
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(123.45, 123)]
+        [InlineData(123.88, 124)]
+        [InlineData(123, 123)]
+        [InlineData(0, 0)]
+        [InlineData(-1, -1)]
+        [InlineData(-1.45, -1)]
+        [InlineData(-1.88, -2)]
+        public void Double_ToInt32_converts_as_expected(double value, int expected)
+        {
+            var result = value.ToInt32();
+
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(123, 123)]
+        [InlineData(12, 12)]
+        [InlineData(0, 0)]
+        public void Uint_ToInt32_converts_as_expected(uint value, int expected)
+        {
+            var result = value.ToInt32();
+
+            result.Should().Be(expected);
+        }
+    }
+}
